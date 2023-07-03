3c3df9e41b62958ec5286dcf186b70d080aff178
MlsMoon <1102255102@qq.com>
2023-07-04 00:33:38 +0800

    Update GameApp.cpp

--- Core/private/GameApp.cpp
+++ Core/private/GameApp.cpp
@@ -36,10 +36,14 @@ void GameApp::DrawScene()
 {
     assert(m_pd3dImmediateContext);
     assert(m_pSwapChain);
-    static float blue[4] = { 0.0f, 0.0f, 1.0f, 1.0f };  // RGBA = (0,0,255,255)
+    static float blue[4] = { 0.1f, 0.1f, 0.1f, 1.0f };  // RGBA = (0,0,255,255)
     m_pd3dImmediateContext->ClearRenderTargetView(m_pRenderTargetView.Get(), blue);
     m_pd3dImmediateContext->ClearDepthStencilView(m_pDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 
+    // 绘制三角形
+    m_pd3dImmediateContext->Draw(3, 0);
+
+    //绘制UI
     ImGui::Render();
     // 下面这句话会触发ImGui在Direct3D的绘制
     // 因此需要在此之前将后备缓冲区绑定到渲染管线上
@@ -88,6 +92,79 @@ bool GameApp::InitResources()
     if (!InitShaders())
         return false;
 
+    //载入顶点数据
+    //这里先用一个三角形代替，后续需要载入模型获取顶点数据
+
+    // 设置三角形顶点
+    const BufferStruct::VertexPosColor vertices[] =
+    {
+        { DirectX::XMFLOAT3(0.0f, 0.5f, 0.5f), DirectX::XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+        { DirectX::XMFLOAT3(0.5f, -0.5f, 0.5f), DirectX::XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+        { DirectX::XMFLOAT3(-0.5f, -0.5f, 0.5f), DirectX::XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) }
+    };
+
+    // 顶点缓冲区描述 结构如下：
+    // typedef struct D3D11_BUFFER_DESC
+    // {
+    //     UINT ByteWidth;             // 数据字节数
+    //     D3D11_USAGE Usage;          // CPU和GPU的读写权限相关
+    //     UINT BindFlags;             // 缓冲区类型的标志
+    //     UINT CPUAccessFlags;        // CPU读写权限的指定
+    //     UINT MiscFlags;             // 忽略
+    //     UINT StructureByteStride;   // 忽略
+    // }     D3D11_BUFFER_DESC;
+    
+    // 设置Buffer描述
+    D3D11_BUFFER_DESC vertex_buffer_desc;
+    // 是一个常见的用法，用于将内存块的内容设置为零。
+    //在这个语句中，ZeroMemory 是一个宏定义，它接受两个参数：要清零的内存块的起始地址和内存块的大小。
+    ZeroMemory(&vertex_buffer_desc, sizeof(vertex_buffer_desc));
+    vertex_buffer_desc.Usage = D3D11_USAGE_IMMUTABLE; // 设置 cpu和gpu 的读写权限，不同权限的更新效率不一样
+    vertex_buffer_desc.ByteWidth = sizeof vertices;
+    vertex_buffer_desc.BindFlags = D3D11_BIND_VERTEX_BUFFER; // 设置buffer 类型 这里设置为 vertex buffer
+    vertex_buffer_desc.CPUAccessFlags = 0;
+
+    // ，许多资源（如纹理、顶点缓冲区等）需要在创建时进行初始化，以提供初始的数据。
+    // typedef struct D3D11_SUBRESOURCE_DATA {
+    //     const void* pSysMem;        // 指向初始化数据的指针
+    //     UINT SysMemPitch;           // 数据的行大小（以字节为单位），对于二维纹理数据有效
+    //     UINT SysMemSlicePitch;      // 数据的深度大小（以字节为单位），对于三维纹理数据有效
+    // } D3D11_SUBRESOURCE_DATA;
+    // 新建顶点缓冲区
+    D3D11_SUBRESOURCE_DATA InitData;
+    ZeroMemory(&InitData, sizeof(InitData));
+    InitData.pSysMem = vertices;
+    HR(m_pd3dDevice->CreateBuffer(&vertex_buffer_desc, &InitData, m_pVertexBuffer.GetAddressOf()));
+
+    // ******************
+    // 给渲染管线各个阶段绑定好所需资源
+    //
+
+    // 输入装配阶段的顶点缓冲区设置
+    UINT stride = sizeof(BufferStruct::VertexPosColor);	// 跨越字节数
+    UINT offset = 0;						// 起始偏移量
+
+    m_pd3dImmediateContext->IASetVertexBuffers(0, 1, m_pVertexBuffer.GetAddressOf(), &stride, &offset);
+    
+    // 设置图元类型
+    // 图元类似：定义了顶点数据如何描述一个几何形状
+    m_pd3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+    //设置输入布局
+    m_pd3dImmediateContext->IASetInputLayout(m_pVertexLayout.Get());
+    
+    // 将着色器绑定到渲染管线
+    m_pd3dImmediateContext->VSSetShader(m_pVertexShader.Get(), nullptr, 0);
+    m_pd3dImmediateContext->PSSetShader(m_pPixelShader.Get(), nullptr, 0);
+
+    // ******************
+    // 设置调试对象名
+    //
+    D3D11SetDebugObjectName(m_pVertexLayout.Get(), "VertexPosColorLayout");
+    D3D11SetDebugObjectName(m_pVertexBuffer.Get(), "VertexBuffer");
+    D3D11SetDebugObjectName(m_pVertexShader.Get(), "Trangle_VS");
+    D3D11SetDebugObjectName(m_pPixelShader.Get(), "Trangle_PS");
+    
 
     return true;
 }
